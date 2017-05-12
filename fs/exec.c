60158b0411b126720d6876baa479ad5745fe8d99
Josua Groeger <groegerj@thp.uni-koeln.de>
2017-05-12 15:40:21 +0200

    mainline-3.1 fs/exec.c

--- fs/exec.c
+++ fs/exec.c
@@ -1205,18 +1205,6 @@ void free_bprm(struct linux_binprm *bprm)
 	kfree(bprm);
 }
 
-int bprm_change_interp(char *interp, struct linux_binprm *bprm)
-{
-	/* If a binfmt changed the interp, free it first. */
-	if (bprm->interp != bprm->filename)
-		kfree(bprm->interp);
-	bprm->interp = kstrdup(interp, GFP_KERNEL);
-	if (!bprm->interp)
-		return -ENOMEM;
-	return 0;
-}
-EXPORT_SYMBOL(bprm_change_interp);
-
 /*
  * install the new credentials for this executable
  */
