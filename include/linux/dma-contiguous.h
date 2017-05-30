9e3423029239b0abb9add2dd298b63de2f23aa7c
Josua Groeger <groegerj@thp.uni-koeln.de>
2017-05-30 10:44:23 +0200

    exorcise DMA_CMA out of include/ part2

--- include/linux/dma-contiguous.h
+++ include/linux/dma-contiguous.h
@@ -57,29 +57,6 @@ struct cma;
 struct page;
 struct device;
 
-#ifdef CONFIG_DMA_CMA
-
-/*
- * There is always at least global CMA area and a few optional device
- * private areas configured in kernel .config.
- */
-#define MAX_CMA_AREAS	(1 + CONFIG_CMA_AREAS)
-
-extern struct cma *dma_contiguous_default_area;
-
-void dma_contiguous_reserve(phys_addr_t addr_limit);
-int dma_declare_contiguous(struct device *dev, unsigned long size,
-			   phys_addr_t base, phys_addr_t limit);
-
-int dma_prepare_alloc_from_contiguous(struct device *dev, int count,
-					unsigned int order);
-struct page *dma_alloc_from_contiguous(struct device *dev, int count,
-				       unsigned int order);
-bool dma_release_from_contiguous(struct device *dev, struct page *pages,
-				 int count);
-
-#else
-
 #define MAX_CMA_AREAS	(0)
 
 static inline void dma_contiguous_reserve(phys_addr_t limit) { }
@@ -111,8 +88,6 @@ bool dma_release_from_contiguous(struct device *dev, struct page *pages,
 {
 	return false;
 }
-
-#endif
 
 #endif
 
