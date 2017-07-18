8abb7fab7eb8de34b5955edd87ef713779e66cce
Josua Groeger <groegerj@thp.uni-koeln.de>
2017-07-18 17:32:38 +0200

    include/ upgrade part 15

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -324,19 +324,13 @@ static inline unsigned int cpufreq_get(unsigned int cpu)
 /* query the last known CPU freq (in kHz). If zero, cpufreq couldn't detect it */
 #ifdef CONFIG_CPU_FREQ
 unsigned int cpufreq_quick_get(unsigned int cpu);
+unsigned int cpufreq_quick_get_max(unsigned int cpu);
 #else
 static inline unsigned int cpufreq_quick_get(unsigned int cpu)
 {
 	return 0;
 }
-#endif
-
-#ifdef CONFIG_CPU_FREQ_GOV_ONDEMAND_FLEXRATE
-extern int cpufreq_ondemand_flexrate_request(unsigned int rate_ms,
-					     unsigned int duration);
-#else
-static inline int cpufreq_ondemand_flexrate_request(unsigned int rate_ms,
-						    unsigned int duration)
+static inline unsigned int cpufreq_quick_get_max(unsigned int cpu)
 {
 	return 0;
 }
